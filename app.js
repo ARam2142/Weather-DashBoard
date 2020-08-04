96952fb3c3fb8da14a51f42b58c2fe8cc73d4c02
Andres Ramirez <andresaaramirez@aol.com>
2020-08-03 23:34:37 -0400

    I got info to stay static on page after reload, local storage is up 100%

--- app.js
+++ app.js
@@ -40,155 +40,192 @@ $(document).ready(function() {
     //api key
     var apiKey = 'e7b524fee1d749595b3aa90b8bab1f55';
     var city = "";
-    var searchedCities = [];
-    
-        function loadSearchHistory() {
-            //get saved searchedcities from local storage
-            let savedHistory = JSON.parse(localStorage.getItem("searchedcities"));
-            //if items were retrieved from local storage, update the searchedCities array to it
-            if (!searchedCities) {
-                savedHistory = searchedCities;
-            }
-            displaySearchHistory();
-            console.log(searchedCities)
-        }   
+    var savedData = localStorage.getItem("searchedCities")
+    var searchedCities;
+    //worked on with tutor
+    if(savedData){
+        searchedCities = JSON.parse(savedData);
+        displaySearchHistory();
+    }
+    else {
+        searchedCities = [];
+    }
+
+    function displaySearchHistory() {
+        cityRows.empty();
+        searchedCities.forEach(function (city) {
+            
+            let divCityEl = $("<div></div>").addClass('col-12 city');
+            //console.log(divCityEl);
+
+            let buttonCityEl = $('<button></button>').addClass('btn btn-light').text(city);
+            //console.log(buttonCityEl);
+
+            divCityEl.append(buttonCityEl);
+            //console.log(divCityEl.append(buttonCityEl));
+
+            cityRows.append(divCityEl);
+            //console.log( cityRows.append(divCityEl));
+            
+        });
+    }
+
+        //load the searchhistory to storage
+        // function loadSearchHistory() {
+        //     //get saved searchedcities from local storage
+        //     var savedHistory = JSON.parse(localStorage.getItem("searchedCities"));
+        //     //if items were retrieved from local storage, update the searchedCities array to it
+        //     if (!searchedCities) {
+        //         savedHistory = searchedCities;
+                
+        //     } 
+        //     //render searchedcities to the DOM
+        //     displaySearchHistory();
+        //     console.log(searchedCities)
+        // }   
         
         //save search results
         function saveSearchResults() {
-            searchedCities.push(city);
-            inputCity.value = ""
+            //worked on with tutor
+            const prevSearchIndex = searchedCities.indexOf(city);
+            const notSearched = prevSearchIndex === -1;
+            if(notSearched){
+            searchedCities.unshift(city);
+            //strinify and set "cities" in local storage to searchedcities array
             localStorage.setItem('searchedCities', JSON.stringify(searchedCities));
-            displaySearchHistory();
+            //localStorage.clear();
+            }
+            else {//worked on with tutor
+                const removed = searchedCities.splice(prevSearchIndex, 1);
+                searchedCities.unshift(removed.pop());
+            }
         }
 
-        function displaySearchHistory() {
-            cityRows.empty();
-            searchedCities.forEach(function () {
-                let divCityEl = $("<div></div>").addClass('col-12 city');
-                //console.log(divCityEl);
-    
-                let buttonCityEl = $('<button></button>').addClass('btn btn-light');
-                buttonCityEl.text(city);
-                //console.log(buttonCityEl);
-    
-                divCityEl.append(buttonCityEl);
-                //console.log(divCityEl.append(buttonCityEl));
-    
-                cityRows.append(divCityEl);
-                //console.log( cityRows.append(divCityEl));
-      
-            });
-            
-        }
-    
-        //click button to enter city that will return forecast result
-        searchButton.on('click', function(e) {
-            displaySearchHistory();
-            saveSearchResults();
-            e.preventDefault();
-            city = inputCity.val();
-            if(!city.length){
-                return;
-            }
+        //make a button that will show previous clicked city
+       
+
+        function apiCalls() {
+
+            //AJAX Calls below
             $.ajax({ 
                 url:'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, 
                 method: "GET"
             })
-                .then(function(weatherdata){
-                    //Here we have initial weather data
-                    cityName.text(weatherdata.name);
-                    let apiIcon = weatherdata.weather[0].icon;
-                    weatherIconEl.attr( 'src', "http://openweathermap.org/img/w/" + apiIcon + '.png');   
-                    currentDate.text(moment().format('l'));
-                    temperatureEl.text(((Math.round(weatherdata.main.temp - 273.15) * 9/5 + 32)));
-                    humidityEl.text(weatherdata.main.humidity);
-                    windSpeedEl.text(weatherdata.wind.speed);
-                    console.log(weatherdata)
-                    
-    
-                    let lat = weatherdata.coord.lat;
-                    let lon = weatherdata.coord.lon;
-                    //DO 5 day fetch down here
-                    $.ajax({
-                        url:'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&appid=' + apiKey,
-                        method: "GET"
-                    })
-
-                    .then(function(fiveDayForcast) {
-                        console.log(fiveDayForcast);
-                        uvIndexEl.text((fiveDayForcast.current.uvi));
-
-                         //0-2 is green
-                         if (fiveDayForcast.current.uvi < 2.9) {
-                            $(uvIndexEl).addClass("green");
-                        }
+            .then(function(weatherdata){
+                //Here we have initial weather data
+                cityName.text(weatherdata.name);
+                let apiIcon = weatherdata.weather[0].icon;
+                weatherIconEl.attr( 'src', "http://openweathermap.org/img/w/" + apiIcon + '.png');   
+                currentDate.text(moment().format('l'));
+                temperatureEl.text(((Math.round(weatherdata.main.temp - 273.15) * 9/5 + 32)));
+                humidityEl.text(weatherdata.main.humidity);
+                windSpeedEl.text(weatherdata.wind.speed);
+                console.log(weatherdata)
                 
-                        //3-5 is yellow
-                        else if (fiveDayForcast.current.uvi >= 3 && fiveDayForcast.current.uvi < 6) {
-                            $(uvIndexEl).addClass("yellow");
-                        }
-                        
-                        //6-7 is orange
-                        else if (fiveDayForcast.current.uvi >= 6 && fiveDayForcast.current.uvi < 8) {
-                            $(uvIndexEl).addClass("orange");
-                        } 
-                        
-                        //8-10 is red
-                        else if (fiveDayForcast.current.uvi >= 8 && fiveDayForcast.current.uvi < 11) {
-                            $(uvIndexEl).addClass("red");
-                        }
-
-                        //above 11 is violet
-                        else  {
-                            $(uvIndexEl).addClass("violet");
-                        }
-    
-                        //forcast day 1
-                        forcastOneEl.text(moment().add(1, 'd').format('MM-DD-YYYY'));
-                        let weatherIcon1 = fiveDayForcast.daily[0].weather[0].icon;
-                        iconDay1El.attr('src', 'https://openweathermap.org/img/wn/' + weatherIcon1 + '.png');
-                        tempForcast1.text(Math.round(fiveDayForcast.daily[0].temp.day - 273.15) * 9/5 + 32);
-                        humidityForcast1.text(fiveDayForcast.daily[0].humidity);
-                        
-                        
-                        //forcast day 2
-                        forcastTwoEl.text(moment().add(2, 'd').format('MM-DD-YYYY'));
-                        let weatherIcon2 = fiveDayForcast.daily[1].weather[0].icon
-                        iconDay2El.attr('src', 'https://openweathermap.org/img/wn/' + weatherIcon2 + '.png');
-                        tempForcast2.text(Math.round(fiveDayForcast.daily[1].temp.day - 273.15) * 9/5 + 32);
-                        humidityForcast2.text(fiveDayForcast.daily[1].humidity);
-    
-                        //forcast day 3
-                        forcastThreeEl.text(moment().add(3, 'd').format('MM-DD-YYYY'));
-                        let weatherIcon3 = fiveDayForcast.daily[2].weather[0].icon
-                        iconDay3El.attr('src', 'https://openweathermap.org/img/wn/' + weatherIcon3 + '.png');
-                        tempForcast3.text(Math.round(fiveDayForcast.daily[2].temp.day - 273.15) * 9/5 + 32);
-                        humidityForcast3.text(fiveDayForcast.daily[2].humidity);
-    
-                        //forcast day 4
-                        forcastFourEl.text(moment().add(4, 'd').format('MM-DD-YYYY'));
-                        let weatherIcon4 = fiveDayForcast.daily[3].weather[0].icon
-                        iconDay4El.attr('src', 'https://openweathermap.org/img/wn/' + weatherIcon4 + '.png');
-                        tempForcast4.text(Math.round(fiveDayForcast.daily[3].temp.day - 273.15) * 9/5 + 32);
-                        humidityForcast4.text(fiveDayForcast.daily[3].humidity);
-    
-                        //forcast day 5
-                        forcastFiveEl.text(moment().add(5, 'd').format('MM-DD-YYYY'));
-                        let weatherIcon5 = fiveDayForcast.daily[4].weather[0].icon
-                        iconDay5El.attr('src', 'https://openweathermap.org/img/wn/' + weatherIcon5 + '.png');
-                        tempForcast5.text(Math.round(fiveDayForcast.daily[4].temp.day - 273.15) * 9/5 + 32);
-                        humidityForcast5.text(fiveDayForcast.daily[4].humidity);
-                        
-                    })
-    
+                
+                let lat = weatherdata.coord.lat;
+                let lon = weatherdata.coord.lon;
+                //DO 5 day fetch down here
+                $.ajax({
+                    url:'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&appid=' + apiKey,
+                    method: "GET"
                 })
-                .catch(function(err){
+                
+                .then(function(fiveDayForcast) {
+                    console.log(fiveDayForcast);
+                    uvIndexEl.text((fiveDayForcast.current.uvi));
+                    
+                    //0-2 is green
+                    if (fiveDayForcast.current.uvi < 2.9) {
+                        $(uvIndexEl).addClass("green");
+                    }
+                    
+                    //3-5 is yellow
+                    else if (fiveDayForcast.current.uvi >= 3 && fiveDayForcast.current.uvi < 6) {
+                        $(uvIndexEl).addClass("yellow");
+                    }
+                    
+                    //6-7 is orange
+                    else if (fiveDayForcast.current.uvi >= 6 && fiveDayForcast.current.uvi < 8) {
+                        $(uvIndexEl).addClass("orange");
+                    } 
+                    
+                    //8-10 is red
+                    else if (fiveDayForcast.current.uvi >= 8 && fiveDayForcast.current.uvi < 11) {
+                        $(uvIndexEl).addClass("red");
+                    }
+                    
+                    //above 11 is violet
+                    else  {
+                        $(uvIndexEl).addClass("violet");
+                    }
+                    
+                    //forcast day 1
+                    forcastOneEl.text(moment().add(1, 'd').format('MM-DD-YYYY'));
+                    let weatherIcon1 = fiveDayForcast.daily[0].weather[0].icon;
+                    iconDay1El.attr('src', 'https://openweathermap.org/img/wn/' + weatherIcon1 + '.png');
+                    tempForcast1.text(Math.round(fiveDayForcast.daily[0].temp.day - 273.15) * 9/5 + 32);
+                    humidityForcast1.text(fiveDayForcast.daily[0].humidity);
+                    
+                    
+                    //forcast day 2
+                    forcastTwoEl.text(moment().add(2, 'd').format('MM-DD-YYYY'));
+                    let weatherIcon2 = fiveDayForcast.daily[1].weather[0].icon
+                    iconDay2El.attr('src', 'https://openweathermap.org/img/wn/' + weatherIcon2 + '.png');
+                    tempForcast2.text(Math.round(fiveDayForcast.daily[1].temp.day - 273.15) * 9/5 + 32);
+                    humidityForcast2.text(fiveDayForcast.daily[1].humidity);
+                    
+                    //forcast day 3
+                    forcastThreeEl.text(moment().add(3, 'd').format('MM-DD-YYYY'));
+                    let weatherIcon3 = fiveDayForcast.daily[2].weather[0].icon
+                    iconDay3El.attr('src', 'https://openweathermap.org/img/wn/' + weatherIcon3 + '.png');
+                    tempForcast3.text(Math.round(fiveDayForcast.daily[2].temp.day - 273.15) * 9/5 + 32);
+                    humidityForcast3.text(fiveDayForcast.daily[2].humidity);
+                    
+                    //forcast day 4
+                    forcastFourEl.text(moment().add(4, 'd').format('MM-DD-YYYY'));
+                    let weatherIcon4 = fiveDayForcast.daily[3].weather[0].icon
+                    iconDay4El.attr('src', 'https://openweathermap.org/img/wn/' + weatherIcon4 + '.png');
+                    tempForcast4.text(Math.round(fiveDayForcast.daily[3].temp.day - 273.15) * 9/5 + 32);
+                    humidityForcast4.text(fiveDayForcast.daily[3].humidity);
+                    
+                    //forcast day 5
+                    forcastFiveEl.text(moment().add(5, 'd').format('MM-DD-YYYY'));
+                    let weatherIcon5 = fiveDayForcast.daily[4].weather[0].icon
+                    iconDay5El.attr('src', 'https://openweathermap.org/img/wn/' + weatherIcon5 + '.png');
+                    tempForcast5.text(Math.round(fiveDayForcast.daily[4].temp.day - 273.15) * 9/5 + 32);
+                    humidityForcast5.text(fiveDayForcast.daily[4].humidity);
+                    
+                })
             });
-    });
-    
-});
-    
-    /*
+        }
+
+        //Jquery way of adding an event listener on an element that will not be in the DOM until later
+        $(document).on('click', '.city',function(evt){
+            console.log('NOw I woork always')
+        })
+
+
+
+
+        //click button to enter city that will return forecast result
+        searchButton.on('click', function(e) {
+            city = inputCity.val();
+            inputCity.val('');
+            if(!city.length){
+                return;
+            }
+            apiCalls();
+            //e.preventDefault();
+           
+                //Add to searched Cites
+                //Add to local stoarage
+                saveSearchResults();
+                displaySearchHistory();
+            });    
+        });
+        
+        /*
     WHEN I click on a city in the search history
     THEN I am again presented with current and future conditions for that city
     WHEN I open the weather dashboard
